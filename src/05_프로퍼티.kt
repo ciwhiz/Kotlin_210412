339b1b5733cbdebe0b1670b2a24f233e544ef6bf
Chansik Yun <chansigi@gmail.com>
2021-04-12 13:38:24 +0900

    .

---
+++ src/05_프로퍼티.kt
@@ -0,0 +1,16 @@
+// 05_프로퍼티.kt
+package ex5
+
+// 프로퍼티(Property)
+// => 접근자 메소드를 자동으로 생성하는 기술
+//  var: getter + setter
+//  val: getter
+
+class User(val name: String, var age: Int)
+
+fun main() {
+    val user = User("Tom", 42)
+
+    user.age = 100     // setAge
+    println(user.age)  // getAge
+}
---
+++ src/com/lge/ex5/Sample.java
@@ -0,0 +1,39 @@
+package com.lge.ex5;
+
+/*
+class User {
+    private String name;
+    private int age;
+
+    User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+*/
+
+
+import ex5.User;
+
+public class Sample {
+    public static void main(String[] args) {
+        User user = new User("Tom", 42);
+
+    }
+}
