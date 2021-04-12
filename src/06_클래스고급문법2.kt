80e713ba4604ec00e4017677695b1232ae0b737a
Chansik Yun <chansigi@gmail.com>
2021-04-12 15:05:06 +0900

    .

---
+++ src/06_클래스고급문법2.kt
@@ -0,0 +1,42 @@
+// 06_클래스고급문법2.kt
+package ex6_2
+
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
+import java.io.Serializable
+
+interface State : Serializable
+
+// property
+interface View {
+    var state: State
+}
+
+class Button : View {
+    // Kotlin
+    //  - 기본이 Nested class 입니다.
+    //  - inner 키워드를 통해 내부 클래스를 만들 수 있습니다.
+    inner class ButtonState(val name: String) : State
+
+    override var state: State
+        get() {
+            return ButtonState("Button")
+        }
+        set(value) {
+            // ...
+        }
+}
+
+// Kotlin - "Checked Exception"이 존재하지 않습니다.
+//  => 예외 처리 구문이 강제되지 않습니다.
+//  => 개발자가 예외 처리에 대해서 신경써야 합니다.
+fun main() {
+    val button = Button()
+
+    val fos = FileOutputStream("button2.dat")
+    val oos = ObjectOutputStream(fos)
+
+    val state = button.state
+    oos.writeObject(state)
+}
+
--- src/com/lge/ex6/Sample.java
+++ src/com/lge/ex6/Sample.java
@@ -1,5 +1,57 @@
 package com.lge.ex6;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+interface State extends Serializable {
+}
+
+interface View {
+    State getCurrentState();
+
+    void restoreState(State s);
+}
+
+class Button implements View {
+
+    // 내부 클래스(Inner class): 일반적으로 사용할 때 주의가 필요합니다.
+    //  : 외부 객채에 대한 암묵적인 참조가 내부 필드로 생성합니다.
+
+    // class ButtonState implements State {
+
+    // 중첩 클래스(Nested class)
+    // static class ButtonState implements State {
+    static class ButtonState implements State {
+        // Button outerClassObj;
+        String name = "Button";
+        int x = 10;
+        int y = 20;
+    }
+
+    @Override
+    public State getCurrentState() {
+        return new ButtonState(); // this
+    }
+
+    @Override
+    public void restoreState(State s) {
+        // ...
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) throws Exception {
+        Button button = new Button();
+
+        FileOutputStream fos = new FileOutputStream("button.dat");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        State s = button.getCurrentState();
+        oos.writeObject(s);
+    }
+}
+/*
 interface Clickable {
     void click();
 
@@ -21,3 +73,4 @@ public class Sample {
 
     }
 }
+*/
