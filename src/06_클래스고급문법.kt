e2ab4062ff9977f348dd3b2ffd4a625c1233dcaa
Chansik Yun <chansigi@gmail.com>
2021-04-12 14:15:58 +0900

    .

--- src/06_클래스고급문법.kt
+++ src/06_클래스고급문법.kt
@@ -10,16 +10,34 @@ package ex6
 //    => 인터페이스에 기본 구현을 제공하는 것은 유용하다.
 //    => Java 8 부터 인터페이스에 기본 구현을 제공할 수 있습니다.
 //       default method / defender method
+
+// 2. 여러 개의 인터페이스를 구현할 때, 구현의 충돌이 발생할 수 있습니다.
 interface Clickable {
     fun click()
 
     fun showOff() {
-
+        println("Clickable")
     }
 }
 
-// class Button implements Clickable
-class Button: Clickable {
+interface Focusable {
+    fun focus()
+
+    fun showOff() {
+        println("Focusable")
+    }
+}
+
+// class Button implements Clickable implements Focusable
+class Button : Clickable, Focusable {
+    override fun showOff() {
+        super<Clickable>.showOff()  // Clickable.showOff
+        super<Focusable>.showOff()  // Focusable.showOff
+    }
+
+    override fun focus() {
+    }
+
     override fun click() {
         println("Button click")
     }
@@ -28,6 +46,8 @@ class Button: Clickable {
 fun main() {
     val button = Button()
     button.click()
+
+    button.showOff()
 }
 
 
