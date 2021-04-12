4b2ef2b3f3cbdc7f0c3626a3a356b0d1873d5841
Chansik Yun <chansigi@gmail.com>
2021-04-12 14:01:10 +0900

    .

--- src/05_프로퍼티.kt
+++ src/05_프로퍼티.kt
@@ -50,7 +50,6 @@ fun main() {
 
 //  2) Backing Field가 없는 프로퍼티: 메소드의 '가독성'을 높이기 위해 사용한다.
 class User(var firstName: String, var lastName: String) {
-
     // Backing Field가 없는 프로퍼티 - 메소드
     //  - Swift: 계산형 프로퍼티
 
@@ -59,6 +58,7 @@ class User(var firstName: String, var lastName: String) {
 
     var fullName: String
         get() {
+            // firstName = "xxx"
             return "$firstName, $lastName"
         }
         set(value) {
@@ -66,6 +66,14 @@ class User(var firstName: String, var lastName: String) {
             firstName = arr[0]
             lastName = arr[1]
         }
+
+    // 아래처럼 다른 타입으로 변환하는 작업을 프로퍼티의 형태로 제공하는 것은 코틀린의 '컨벤션'에 어긋납니다.
+    /*
+    val description: String
+        get() {
+            return "User(firstName=$firstName, lastName=$lastName)"
+        }
+    */
 }
 
 fun main() {
@@ -75,9 +83,20 @@ fun main() {
     user.fullName = "Soonshin, Lee"
     println(user.firstName)
     println(user.lastName)
+
+    // println(user.description)
 }
 
-
+// 프로퍼티 vs 메소드
+// 1. 복잡한 코드 - 메소드
+// 2. 시간이 오래 걸리는 작업 - 메소드
+// 3. 프로퍼티의 getter 안에서 다른 프로퍼티의 값을 변경하는 행위는 좋지 않습니다.
+// 4. 프로퍼티 안에서 예외가 발생하면 안됩니다. - 메소드
+// 5. 다른 타입의 값이 필요한 경우 - 메소드
+//     : toString()
+//       toLong()
+//       toPerson()
+// 6. 객체의 복사본 - 메소드
 
 
 
