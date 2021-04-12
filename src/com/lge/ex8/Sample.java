2d6cdd7e2397663168af26d82932e0aa404cd808
Chansik Yun <chansigi@gmail.com>
2021-04-12 16:17:41 +0900

    .

--- src/com/lge/ex8/Sample.java
+++ src/com/lge/ex8/Sample.java
@@ -1,5 +1,6 @@
 package com.lge.ex8;
 
+/*
 class Point implements Cloneable {
     private int x;
     private int y;
@@ -27,6 +28,7 @@ class Point implements Cloneable {
         return null;
     }
 }
+*/
 
 // Java에서 객체 복제를 구현하는 방법 - Object.clone
 // 1. Object.clone 오버라이드
@@ -40,10 +42,44 @@ public interface Cloneable {
 }
 */
 
+// Object
+//   |
+//  XXX   - clone() X
+//   |
+//  Point - clone()
+// clone의 문제점: 계층 구조의 모든 클래스가 clone을 구현해야 한다.
+// => clone을 이용하지 말자.
+//    "복사 생성자"를 사용하는 것이 좋다.
+//     - 자신과 동일한 타입을 생성자의 인자로 전달받는다.
+class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // 복사 생성자
+    public Point(Point rhs) {
+        x = rhs.x;
+        y = rhs.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
 public class Sample {
     public static void main(String[] args) {
         Point p1 = new Point(10, 20);
-        Point p2 = p1.clone();
+        // Point p2 = p1.clone();
+        Point p2 = new Point(p1);
 
         System.out.println(p1);
         System.out.println(p2);
