78de8045db6ef69f97ec2116c5b298c9b6d6a716
Chansik Yun <chansigi@gmail.com>
2021-04-12 13:44:18 +0900

    .

--- src/05_프로퍼티.kt
+++ src/05_프로퍼티.kt
@@ -6,7 +6,28 @@ package ex5
 //  var: getter + setter
 //  val: getter
 
-class User(private val name: String, var age: Int)
+// class User(private val name: String, var age: Int)
+// - getter / setter 내부에서 필드의 값을 읽어올 수 있는 field 라는 키워드를 사용해야 합니다.
+
+class User {
+    val name: String
+        get() {
+            return "name: $field"
+        }
+
+    var age: Int
+        get() {
+            return field + 10
+        }
+        set(value) {
+            field = value - 1
+        }
+
+    constructor(name: String, age: Int) {
+        this.name = name
+        this.age = age
+    }
+}
 
 fun main() {
     val user = User("Tom", 42)
@@ -14,5 +35,5 @@ fun main() {
     user.age = 100     // setAge
     println(user.age)  // getAge
 
-    // println(user.name)
+    println(user.name)
 }
