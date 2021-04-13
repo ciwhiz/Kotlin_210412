81e8ad5d98550b9e53266d9cafa9a5d3948ac929
Chansik Yun <chansigi@gmail.com>
2021-04-13 17:01:02 +0900

    .

--- src/15_고차함수4.kt
+++ src/15_고차함수4.kt
@@ -4,7 +4,10 @@ package ex15_4
 // 1) 함수를 인자로 전달하는 함수
 // => 다양한 시라니오 동작하는 함수의 코드 중복을 없앨 수 있다.
 
-// 2) 함수를 반환하는 함수?
+// 2) 함수를 반환하는 함수
+// => 실행 시간에 함수를 생성할 수 있습니다.
+//   - 함수 합성
+//   - 커링
 
 // () -> (String) -> String)
 //  : -> 오른쪽 결합을 합니다.
@@ -17,7 +20,6 @@ fun foo(): (String) -> String {
 
 fun main() {
     // val fn: () -> (String) -> String = ::foo
-
     val result: (String) -> String = foo()  // 함수가 반환합니다.
 
     val result2: String = result("hello")
