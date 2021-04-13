e97e85d6e68f80e08906a2b7545b8053e9f97ac2
Chansik Yun <chansigi@gmail.com>
2021-04-13 11:12:09 +0900

    .

--- src/10_위임2.kt
+++ src/10_위임2.kt
@@ -6,6 +6,7 @@ import kotlin.reflect.KProperty
 // 프로퍼티 위임
 //  : 프로퍼티에 대한 getter / setter에 대한 호출을 다른 객체에게 위임한다.
 
+/*
 class SampleDelegate {
     operator fun getValue(thisRef: User, property: KProperty<*>): String {
         return "Tom"
@@ -15,14 +16,40 @@ class SampleDelegate {
         println("setValue - $newValue")
     }
 }
+*/
 
 // Type 'SampleDelegate' has no method 'getValue(User, KProperty<*>)'
 // and thus it cannot serve as a delegate
 // Type 'SampleDelegate' has no method 'setValue(User, KProperty<*>, String)'
 // and thus it cannot serve as a delegate for var (read-write property)
-class User {
-    var name: String by SampleDelegate()
+
+
+interface OnValueChanged {
+    fun onChangedValue(old: String, new: String)
 }
+
+class SampleDelegate(var field: String, var onValueChanged: OnValueChanged? = null) {
+    operator fun getValue(thisRef: User, property: KProperty<*>): String {
+        return field
+    }
+
+    operator fun setValue(thisRef: User, property: KProperty<*>, newValue: String) {
+        val oldValue = field
+        field = newValue
+
+        onValueChanged?.onChangedValue(oldValue, newValue)
+    }
+}
+
+class User {
+    // Backing Field 가 없는 프로퍼티
+    var name: String by SampleDelegate("Alice", object : OnValueChanged {
+        override fun onChangedValue(old: String, new: String) {
+            println("name: $old -> $new")
+        }
+    })
+}
+
 
 fun main() {
     val user = User()
