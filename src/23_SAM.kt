475944bf7a77ad1c96bb80d5a47069d071568c5f
Chansik Yun <chansigi@gmail.com>
2021-04-14 17:00:30 +0900

    .

--- src/23_SAM.kt
+++ src/23_SAM.kt
@@ -1,8 +1,14 @@
 // 23_SAM
 package ex23
 
-import com.lge.ex23.OnClickListener
+// import com.lge.ex23.OnClickListener
 
+// Kotlin - 1.4
+//  : 코틀린의 인터페이스가 하나의 메소드를 가진 경우에도 SAM 지원을 사용할 수 있습니다.
+//   "fun interface"
+fun interface OnClickListener {
+    fun onClick()
+}
 
 class Button {
     var onClickListener: OnClickListener? = null
