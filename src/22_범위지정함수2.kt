1c6c98ed4e3602a00ba4e4e60a887d578a2b15c2
Chansik Yun <chansigi@gmail.com>
2021-04-14 16:35:38 +0900

    .

--- src/22_범위지정함수2.kt
+++ src/22_범위지정함수2.kt
@@ -1,3 +1,21 @@
 // 22_범위지정함수2.kt
 package ex22
 
+// 코틀린은 Try with resources 문법을 지원하지 않습니다.
+class Resource : AutoCloseable {
+    override fun close() {
+        println("Resource close")
+    }
+
+    fun foo() {
+        println("Resource foo")
+    }
+}
+
+// use - AutoClosable
+fun main() {
+    val res = Resource()
+    res.use { e ->
+        e.foo()
+    }
+}
