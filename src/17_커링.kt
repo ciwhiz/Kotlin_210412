5c19485d470c2975a47d7bbf310b188a5fd71113
Chansik Yun <chansigi@gmail.com>
2021-04-14 10:07:01 +0900

    .

---
+++ src/17_커링.kt
@@ -0,0 +1,44 @@
+package ex17
+
+// 커링(Currying)
+//  : 다중 인수를 갖는 함수를 단일 인수를 갖는 함수들의 함수열로 바꾸는 작업
+// 용도
+// - 함수의 인자에 대해서 값을 고정하고 싶다. - bind
+// - 함수 호출 시점을 지연할 수 있다.
+
+// 일반 버전
+// sum(10, 20, 30, 40)
+// => 110
+
+// 커링 버전
+// sum(10)(20)(30)(40)
+// => 110
+
+/*
+fun sum(x: Int, y: Int, z: Int) = x + y + z
+
+// Scala
+fun sum(x: Int)(y: Int)(z: Int) = x + y + z
+*/
+
+fun sum2(x: Int, y: Int): Int = x + y
+
+fun sum2(x: Int): (y: Int) -> Int = { y: Int ->
+    x + y
+}
+
+fun main() {
+    // var result = sum2(10, 20)
+    // println(result)
+
+    val result = sum2(10)(20)
+    println(result)
+
+    val sum10 = sum2(10)
+
+    println(sum10(20))  // 30
+    println(sum10(30))  // 40
+}
+
+
+
