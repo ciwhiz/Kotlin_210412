321f72a5be004984de9265e077d2e5ec7b067188
Chansik Yun <chansigi@gmail.com>
2021-04-15 17:01:46 +0900

    .

--- android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity3.kt
+++ android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity3.kt
@@ -1,17 +1,122 @@
 package io.yoondev.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import io.yoondev.firstapp.databinding.MainActivity3Binding
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+
+
+/*
+{
+  "login": "JakeWharton",
+  "id": 66577,
+  "avatar_url": "https://avatars.githubusercontent.com/u/66577?v=4",
+  "name": "Jake Wharton",
+  "company": "Square",
+  "email": null,
+}
+*/
+
+// HTTPRequest / Volley ...
+//  => OKHttpClient
+
+// DTO(Data Transfer Object)
+//  => null 가능성이 있는 타입은 Nullable로 만들어야 합니다.
+//  => gson
+//     : "null"
+//  => proguard 예외로 등록해야 합니다. - Reflection
+//  dto.User
+data class User(
+    val login: String,
+    val id: Int,
+    val avatarUrl: String,
+    val name: String?,
+    val company: String?,
+    val email: String?,
+)
 
 
 class MainActivity3 : AppCompatActivity() {
-    // private val binding: MainActivity2Binding by viewBinding()
+    private val binding: MainActivity3Binding by viewBinding()
+
+    // private static final String TAG = "MainActivity3"
+    // private static final String TAG = MainActivity3.class.getSimpleName()
+
+    // 상수
+    // - 컴파일 타임 상수
+    // - 런타임 상수: 메모리가 할당되어 있는 언어적으로 변경이 불가능하다.
+
+    companion object {
+
+        // 컴파일 타임 상수 - const
+        // private const val TAG1 = "MainActivity3"
+
+        // 컴파일 타임 상수 처리가 불가능합니다.
+        private val TAG = MainActivity3::class.java.simpleName
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // api.github.com/users/JakeWharton
+        binding.loadButton.setOnClickListener {
+
+            // 1. OKHttpClient 객체 생성
+            val client = OkHttpClient.Builder().apply {
+                /*
+                val loggingInterceptor = HttpLoggingInterceptor()
+                loggingInterceptor.level = HttpLoggingInterceptor.Level.BASIC
+                addInterceptor(loggingInterceptor)
+                */
+
+                addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BASIC
+                })
+
+            }.build()
+
+            // 2. Request 생성
+            //  : url / HTTP method / body / param
+            val request = Request.Builder().apply {
+                get()
+                url("https://api.github.com/users/JakeWharton")
+            }.build()
+
+
+            // 3. 'Call' 을 생성합니다.
+            //  '동기적' vs '비동기적'
+            //  동기: execute()
+            // 비동기: enqueue()
+            val call = client.newCall(request)
+
+            // 4. 동기적 호출(execute) - Response
+            val response: Response = call.execute()
+
+            // 5. Response
+            //  : HTTP status code
+            //   200 ~ 299: OK
+            //   400 ~ 499: Client Error!
+            //   500 ~ 599: Server Error!
+
+
+            // if (response.code in 200..299) {
+            if (response.isSuccessful) {
+
+                response.body?.let { body ->
+                    val json = body.string()
+                    Log.e(TAG, "Response: $json")
+                }
+
+            }
+
+
+        }
     }
-}
 }
 
 
