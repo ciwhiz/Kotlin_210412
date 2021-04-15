16f584dccf06005e95252c4a1045aba80f22ca01
Chansik Yun <chansigi@gmail.com>
2021-04-15 16:16:24 +0900

    .

---
+++ android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity3.kt
@@ -0,0 +1,20 @@
+package io.yoondev.firstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity3 : AppCompatActivity() {
+    // private val binding: MainActivity2Binding by viewBinding()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+}
+
+
+
+
+
