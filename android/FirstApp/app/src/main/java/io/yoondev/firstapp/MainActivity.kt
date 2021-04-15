678660b8c853ff57e3f5d2aec163bb5514bfcc02
Chansik Yun <chansigi@gmail.com>
2021-04-15 12:17:05 +0900

    .

--- android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity.kt
+++ android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity.kt
@@ -2,10 +2,27 @@ package io.yoondev.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+//      Project-level: build.gradle
+//  Module-level(app): build.gradle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // 1. findViewById
+        //  Java: ButterKnife
+        // val textView = findViewById<TextView>(R.id.textView)
+        // textView.text = "Hello"
+
+        // 2. kotlin-android-extension
+        textView.text = "Hello"
+        button.setOnClickListener {
+            textView.text = "OK"
+        }
     }
 }
