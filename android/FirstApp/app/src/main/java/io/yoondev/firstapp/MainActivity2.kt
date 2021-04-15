92c37c626b12bb666a9a8b05ab0d29f2b955adce
Chansik Yun <chansigi@gmail.com>
2021-04-15 15:15:39 +0900

    .

---
+++ android/FirstApp/app/src/main/java/io/yoondev/firstapp/MainActivity2.kt
@@ -0,0 +1,74 @@
+package io.yoondev.firstapp
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import io.yoondev.firstapp.databinding.*
+import kotlin.properties.Delegates
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+
+// Property Delegate
+class AcitivityBindingDelegate<T : ViewBinding>(
+    private val bindingClass: Class<T>,
+    val activity: Activity
+) : ReadOnlyProperty<Activity, T> {
+
+    private var binding: T? = null
+
+    override fun getValue(thisRef: Activity, property: KProperty<*>): T {
+        binding?.let {
+            return it
+        }
+
+        // inflate(LayoutInflater)
+        val inflateMethod = bindingClass.getMethod(
+            "inflate",
+            LayoutInflater::class.java
+        )
+
+        @Suppress("UNCHECKED_CAST")
+        binding = inflateMethod.invoke(null, thisRef.layoutInflater) as T
+        thisRef.setContentView(binding!!.root)
+
+        return binding!!
+    }
+}
+
+
+class MainActivity2 : AppCompatActivity() {
+
+    val binding: MainActivityBinding by AcitivityBindingDelegate(
+        MainActivityBinding::class.java,
+        this
+    )
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding.nameTextView.text = "hello"
+        binding.okButton.setOnClickListener {
+            Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+}
+
+
+
+
+
+
+
